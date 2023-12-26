c8a27c47923439dcb37d859442dca9555b74af23
Edward Ramos <edward.ramos@procer.com.br>
2023-12-26 10:04:50 -0300

    feat: gradlew build sucess

--- android/src/main/java/com/viewtrak/plugins/usbserial/UsbSerial.java
+++ android/src/main/java/com/viewtrak/plugins/usbserial/UsbSerial.java
@@ -9,6 +9,7 @@ import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.widget.Toast;
