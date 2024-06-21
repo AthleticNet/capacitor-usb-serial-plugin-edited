556377630b49635d6ce1f59eba3c3422e7422b17
Edward Ramos <edward.ramos@procer.com.br>
2024-06-21 18:11:52 -0300

    feat: import Collectors

--- android/src/main/java/com/viewtrak/plugins/usbserial/UsbSerial.java
+++ android/src/main/java/com/viewtrak/plugins/usbserial/UsbSerial.java
@@ -34,6 +34,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 
 public class UsbSerial implements SerialInputOutputManager.Listener {
